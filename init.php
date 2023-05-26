8eefcd1ff3db531c60aa145702285c76edc24e80
Dat Tran <tranthanhdat.it@gmail.com>
2023-05-26 14:31:23 +0700

    Update styles

--- init.php
+++ init.php
@@ -30,11 +30,15 @@ function bfx_crypto_map_handler( $atts ) {
     <div class="bfx-crypto-filter">
       <div class="bfx-crypto-filter-bar">
         <div class="search-container">
+          <img src="$asset_url/search.png" width="14" height="13" />
           <input id="bfx-crypto-search-input" type="search" placeholder="Search" />
         </div>
         <button type="button" class="filter-btn" id="bfx-crypto-filter-btn">
           <img src="$asset_url/filter.png" />
           <span>Filter by</span>
+          <div class="arrow">
+            <img src="$asset_url/arrow-down.png" />
+          </div>
         </button>
       </div>
       <div id="bfx-crypto-filter-popup" class="bfx-crypto-filter-popup">
